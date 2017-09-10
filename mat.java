407432cdc3f427828506b1526e0eb82cb8e10c78
Guðjón Ragnar Brynjarsson <gudjonragnar@n142-p49.eduroam.kth.se>
2017-09-10 12:44:27 +0200

    Baum Welch proto

--- mat.java
+++ mat.java
@@ -24,12 +24,24 @@ public class mat {
         }
     }
 
+    public mat(int N,int M,Double initialValue) {
+        nmrOfRows = N;
+        nmrOfColumns = M;
+        matrix = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            Double[] arr = new Double[M];
+            ArrayList<Double> row = new ArrayList<>(Arrays.asList(arr));
+            Collections.fill(row, initialValue);//fills all M entries with 0
+            matrix.add(row);
+        }
+    }
     /*
     Build matrix from Kattis' input lines
     matDesc is on the format:
     1 3 0.3 0.3 0.4
      */
     public mat (String matDesc){
+        matDesc = matDesc.trim();
         String[] i = matDesc.split(" ");
         List<String> items = Arrays.asList(i);
         nmrOfRows = Integer.parseInt(items.get(0));
@@ -54,8 +66,46 @@ public class mat {
         matrix.get(i).set(j,e);
     }
 
+    public void setRow(int rowNmr, List<Double> row){
+        if (row.size() == this.getNmrOfColumns()){
+            matrix.set(rowNmr, row);
+        }
+    }
+
     public List<Double> getRow(int i){
         return matrix.get(i);
+    }
+
+    public mat getColAsMat(int col){
+        mat result = new mat(this.getNmrOfRows(),1);
+        for (int i = 0;i<matrix.size();i++){
+            result.setElement(i,1,this.getElement(i,col));
+        }
+        return result;
+    }
+
+    public mat RowAsCol() {
+        mat result = new mat(this.getNmrOfColumns(),1);
+        if (this.getNmrOfRows() != 1) {
+            throw new java.lang.RuntimeException("Needs to be a single row matrix (row vector)");
+        } else {
+            for (int i=0; i<this.getNmrOfColumns();i++){
+                result.setElement(i,0,this.getElement(0,i));
+            }
+        }
+        return result;
+    }
+
+    public mat ColAsRow() {
+        mat result = new mat(1,this.getNmrOfRows());
+        if (this.getNmrOfColumns() != 1) {
+            throw new java.lang.RuntimeException("Needs to be a single column matrix (column vector)");
+        } else {
+            for (int i=0; i<this.getNmrOfRows();i++){
+                result.setElement(0,i,this.getElement(i,0));
+            }
+        }
+        return result;
     }
 
 
@@ -97,7 +147,7 @@ public class mat {
     }
 
     public mat dotProductColumn(mat multBy, int col){
-        mat result = new mat(this.getNmrOfRows(),multBy.getNmrOfColumns());
+        mat result = new mat(this.getNmrOfRows(),this.getNmrOfColumns());
         if (this.getNmrOfColumns() != multBy.getNmrOfRows()|| this.getNmrOfRows()!=1) {
             throw new java.lang.RuntimeException("Dimensions do not match!");
         } else {
@@ -124,6 +174,7 @@ public class mat {
         for (List v:this.matrix){
             System.out.println(Arrays.toString(v.toArray()));
         }
+        System.out.println();
     }
 
     public static void main(String[] args){
@@ -145,7 +196,7 @@ public class mat {
         g.setElement(0,1,2.0);
         g.setElement(0,2,3.0);
 
-        mat matFromString = new mat("2 3 1 1 1 2 2 2");
+        mat matFromString = new mat("0 3");
         matFromString.product(m).printMatrix();
 
         mat res = m.product(n);
